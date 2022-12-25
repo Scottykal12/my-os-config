aea2df025a896932fcb3f8d1ff0679b8df2e2628
Scott Kalinec <scottykal12@gmail.com>
2022-12-25 10:06:32 +0100

    init commit

---
+++ makemine.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# update
+sudo apt update -y
+sudo apt upgrade -y
+
+# vscode and extentions
+sudo apt install code -y
+$extentions = 'cat VSextentions'
+for $e in $extentions {
+    code --install-extension $e
+}
+
+# rust 
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+# libraries
+$libs = 'cat FLTKlibs'
+for $l in $libs{
+    sudo apt install $l -y
+} 
+
+# python and pip
+sudo apt install python3 && python3-pip
+
+# procesing
+wget https://github.com/processing/processing4/releases/download/processing-1289-4.1.1/processing-4.1.1-linux-x64.tgz
+tar -xzf processing-4.*
+
+
+# chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+
+# PRoject folder
+mkdir ~/Projects 
+
+# git name and email
+git config --global user.name "Scott Kalinec"
+git config --global user.email scottykal12@gmail.com
+
+
+
