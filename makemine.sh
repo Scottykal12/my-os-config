fe9114e60c2e24530090263fdb6c9585ef3016ae
Scott Kalinec <scottykal12@gmail.com>
2023-01-17 08:56:59 +0100

    Proper seperate install based on os

--- makemine.sh
+++ makemine.sh
@@ -3,66 +3,121 @@
 # wget -O - https://raw.githubusercontent.com/scottykal12/my-os-config/main/makemine.sh | sh
 # wget -O - https://raw.githubusercontent.com/scottykal12/my-os-config/main/makemine.sh >> test.sh && chmod 755 test.sh && ./test.sh
 
-ogPWD=&(pwd)
+ogPWD=$(pwd)
 
-# update
-sudo apt update -y
-sudo apt upgrade -y
-sudo apt install curl -y
-sudo apt install git -y
+debInstall() {
+  # update
+  sudo apt update -y
+  sudo apt upgrade -y
+  sudo apt install curl -y
+  sudo apt install git -y
 
-sudo pacman -Sy -q
-sudo pacman -Su -q
-sudo pacman -S curl -q --noconfirm
-sudo pacman -S git -q --noconfirm
+  # config git
+  git config --global user.name Scott Kalinec
+  git config --global user.email scottykal12@gmail.com
 
-git config --global user.name Scott Kalinec
-git config --global user.email scottykal12@gmail.com
+  mkdir Projects
 
-mkdir Projects
+  cd Projects
 
-cd Projects
+  # download all git repos
+  curl "https://api.github.com/users/Scottykal12/repos?page=1&per_page=100" |
+    grep -e 'clone_url*' |
+    cut -d \" -f 4 |
+    xargs -L1 git clone
 
-curl "https://api.github.com/users/Scottykal12/repos?page=1&per_page=100" |
-  grep -e 'clone_url*' |
-  cut -d \" -f 4 |
-  xargs -L1 git clone
+  cd my-os-config
 
-cd my-os-config
-
-# vscode and extentions
-sudo apt install code -y
-sudo snap install code --classic
-sudo pacman -S code -q --noconfirm
-extentionsFILE="VSextentions"
-extentions=$(cat $extentionsFILE)
-for e in $extentions; do
+  # code
+  sudo apt install code -y
+  sudo snap install code --classic
+  extentionsFILE="VSextentions"
+  extentions=$(cat $extentionsFILE)
+  for e in $extentions; do
     code --install-extension $e
-done
+  done
 
-# rust
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+  # rust
+  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
-# libraries
-libsFILE="FLTKlibs"
-libs=$(cat $libsFILE)
-for l in $libs; do
+  # libraries
+  libsFILE="FLTKlibs"
+  libs=$(cat $libsFILE)
+  for l in $libs; do
     sudo apt install $l -y
-    sudo pacman -S $l -q --noconfirm
-done
+  done
 
-# python and pip
-sudo apt install python3 && python3-pip -y
-sudo pacman -S python3 && python3-pip -q --noconfirm
+  # python and pip
+  sudo apt install python3 && python3-pip -y
+
+  # chrome
+  wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+  sudo dpkg -i google-chrome-stable_current_amd64.deb
+
+  # cleanup
+  rm -f google-chrome-stable_current_amd64.deb
+  rm $ogPWD/makemine.sh
+}
+
+archInstall() {
+  #  update
+  sudo pacman -Sy -q
+  sudo pacman -Su -q
+  sudo pacman -S curl -q --noconfirm
+  sudo pacman -S git -q --noconfirm
+
+  git config --global user.name Scott Kalinec
+  git config --global user.email scottykal12@gmail.com
+
+  mkdir Projects
+
+  cd Projects
+
+  curl "https://api.github.com/users/Scottykal12/repos?page=1&per_page=100" |
+    grep -e 'clone_url*' |
+    cut -d \" -f 4 |
+    xargs -L1 git clone
+
+  cd my-os-config
+
+  # vscode and extentions
+  sudo pacman -S code -q --noconfirm
+  extentionsFILE="VSextentions"
+  extentions=$(cat $extentionsFILE)
+  for e in $extentions; do
+    code --install-extension $e
+  done
+
+  # rust
+  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+  # libraries
+  libsFILE="FLTKlibs"
+  libs=$(cat $libsFILE)
+  for l in $libs; do
+    sudo pacman -S $l -q --noconfirm
+  done
+
+  # python and pip
+  sudo pacman -S python3 && python3-pip -q --noconfirm
+
+  # chrome
+  pamac build google-chrome --no-confirm
+
+  rm $ogPWD/makemine.sh
+}
+
+osType=$(cat /etc/os-release | grep -i "ID_LIKE")
+
+if [$osType == *debian*]; then
+  debInstall
+fi
+
+if [$osType == *arch*]; then
+  archInstall
+fi
 
 # procesing
 # wget https://github.com/processing/processing4/releases/download/processing-1289-4.1.1/processing-4.1.1-linux-x64.tgz
 # tar -xzf processing-4.*
 # ~/processing
-
-# chrome
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i google-chrome-stable_current_amd64.deb
-
-rm -f google-chrome-stable_current_amd64.deb
-rm $ogPWD/makemine.sh
