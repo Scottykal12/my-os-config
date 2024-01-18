5ff5ca5fd98308310d7543b9cc6539e4b550db1a
Scott Kalinec <scottykal12@gmail.com>
2024-01-18 18:14:54 +0100

    add emulation

--- makemine.ps1
+++ makemine.ps1
@@ -1,4 +1,4 @@
-New-Item .\config-os
+New-Item -ItemType Directory .\config-os
 Set-Location .\config-os
 
 # install code steam and galaxy
@@ -7,6 +7,16 @@ wingit intall Valve.Steam
 winget install GOG.Galaxy
 winget install Google.Chrome
 
-# emustation
+# emulation
+# retroarch
+Invoke-WebRequest -Uri https://buildbot.libretro.com/stable/1.16.0/windows/x86_64/RetroArch-Win64-setup.exe -OutFile retroarch.exe
+.\retroarch
+
+# emulationstation
 Invoke-WebRequest -Uri https://emulationstation.org/downloads/releases/emulationstation_win32_latest.exe -OutFile emu.exe
 .\emu.exe
+
+Remove-Item -OutFile $env:USERPROFILE\.emulationstation\es_systems.cfg
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/Scottykal12/my-os-config/main/es_systems.cfg -OutFile $env:USERPROFILE\.emulationstation\es_systems.cfg
+
+New-Item -ItemType Directory $env:USERPROFILE\.emulationstation\roms
